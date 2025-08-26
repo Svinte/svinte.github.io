c01273c405a14a33239b216a9e42e8d9a37ece45
Svante Kokkoniemi <kokkoniemisvante@gmail.com>
2025-08-26 11:06:35 +0300

    Layout moderation

--- src/views/common/About/Who.vue
+++ src/views/common/About/Who.vue
@@ -1,12 +1,12 @@
 <template>
     <section id="who" class="row justify-content-center align-items-center gap-5">
+        <figure class="col-md-4">
+            <img src="/images/undraw_personal-goals.svg" class="img-fluid" alt="">
+        </figure>
+
         <div class="col-md-4">
             <h2>Kuka olen?</h2>
             <p>Lyhyt kuvaus itsestäni ja taustastani.</p>
         </div>
-
-        <figure class="col-md-4">
-            <img src="/images/undraw_personal-goals.svg" class="img-fluid" alt="">
-        </figure>
     </section>
 </template>
